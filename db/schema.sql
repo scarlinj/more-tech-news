370f2c9352240c5fc5660fa02d996c7c48c6d425
Stephen Carlin <scarlinj@gmail.com>
2023-04-30 17:17:37 -0700

    initial commit

---
+++ db/schema.sql
@@ -0,0 +1,3 @@
+DROP DATABASE IF EXISTS more_tech_news_db;
+
+CREATE DATABASE more_tech_news_db;
---
+++ server.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const express = require('express');
+const exphbs = require('express-handlebars');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sequelize = require('./config/connection');
+
+const hbs = exphbs.create({});
+ 
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(require('./controllers/'));
+
+sequelize.sync({
+      // change this when you create new databases
+    // "force: true" would cause Sequelize to drop and recreate database tables on startup.  We want database to understand when we change something, 
+    // so this allows for us to make those changes while updating the project.
+    // If we change the value of the force property to true, then the database connection must sync with the model definitions and associations. 
+    // By forcing the sync method to true, we will make the tables re-create if there are any association changes
+    force: false }).then(() => {
+  app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
+});
