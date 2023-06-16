b802986730966c6ca110232cbfb8db01587eb434
Stephen Carlin <scarlinj@gmail.com>
2023-06-15 23:27:20 -0700

    editing connection.js to include host 127.0.0.1:27017

--- config/connection.js
+++ config/connection.js
@@ -13,7 +13,7 @@ if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
 sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: '0.0.0.0:27017',
+  host: '127.0.0.1:27017',
   dialect: 'mysql',
   port: 3306
 });
