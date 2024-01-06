537a0a1fbb8f2547a68aa43e807fba907d884bcf
Stephen Carlin <scarlinj@gmail.com>
2024-01-05 18:04:55 -0800

    added format date to helpers

--- utils/helpers.js
+++ utils/helpers.js
@@ -3,6 +3,15 @@ format_date: date => {
     return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(
     date).getFullYear()}`;
 },
+sort_date: date => {
+  fncompare = function(a,b) {
+  // turn your strings into dates, and then subtract them
+  // to get a value that is either negative, positive, or zero.
+  return new date(b) - new date(a);
+  }
+object.keys(context).sort(fncompare).foreach(function(key) {
+  ret = ret + options.fn({key: key, value: context[key]})
+})},
 format_plural: (word, amount) => {
     if (amount !== 1) {
       return `${word}s`;
