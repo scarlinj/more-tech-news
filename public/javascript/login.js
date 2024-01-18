d4bc8a23e43e23599f1ef84aae3535594785b2ed
Stephen Carlin <scarlinj@gmail.com>
2024-01-17 16:45:49 -0800

    fixed dashboard location in edit-post.  Removed withAuth from user.create in user-routes.  Post works.  User creation works.  Redirect does not work.  Will continue to troubleshoot.

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,30 +1,3 @@
-async function signupFormHandler(event) {
-    event.preventDefault();
-
-    const username = document.querySelector('#username-signup').value.trim();
-    const email = document.querySelector('#email-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
-
-    if (username && email && password) {
-        const response = await fetch('/api/users/login', {
-        method: 'post',
-        body: JSON.stringify({
-            username,
-            email,
-            password
-    }),
-        headers: { 'Content-Type': 'application/json' }
-        });
-
-        // check the response status
-        if (response.ok) {
-        console.log('success');
-        } else {
-        alert(response.statusText);
-        }
-    }
-};
-
 async function loginFormHandler(event) {
     event.preventDefault();
 
@@ -49,7 +22,38 @@ if (email && password) {
 }
 };
 
+async function signupFormHandler(event) {
+    event.preventDefault();
+
+    const username = document.querySelector('#username-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+
+    if (username && email && password) {
+        const response = await fetch('/api/users', {
+        method: 'post',
+        body: JSON.stringify({
+            username,
+            email,
+            password
+    }),
+        headers: { 'Content-Type': 'application/json' }
+        });
+
+        // check the response status
+        // reaches this - see "user created" in console, but error after loading, program crashes
+        if (response.ok) {
+            document.location.replace('/dashboard/');
+            console.log('User created.');
+        } else {
+            alert(response.statusText);
+        }
+    }
+};
+
+
+
 // below cannot be read - already called these above within consts and in .hbs file
 // as of 10/8/2023, removing these prevented log-in from redirecting to homepage.  Added back the below and was able to get it working.
-document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
 document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
+document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
